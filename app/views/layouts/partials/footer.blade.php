66cb72fa0116094980d9546aef9ed22aa3dee9b4
Tarun <tarun@colossal.net>
2015-05-18 17:05:37 -0500

    initial scaffold of site complete

--- app/views/layouts/partials/footer.blade.php
+++ app/views/layouts/partials/footer.blade.php
@@ -1,7 +1,7 @@
 	<div class="footer">
 		<div class="container">
 			<br /><br />
-			<div class="col-md-10 col-md-offset-1">
+			<div class="col-md-12 text-center">
 				<div class="col-md-3">
 					<p>CONNECT STRUCTURAL</p>
 				</div>
